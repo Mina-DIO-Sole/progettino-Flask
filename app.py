2822bf334ec533ad3d14e52ec00c2e970929ce93
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-11-27 09:57:26 +0000

    2 e 3 e mezzo

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 lista_spesa= []
 #rotta principale
-@app.route('/')
-def home():
- return render_template('index.html')
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+   #ttiene elemento dal form
+   elemento = request.form['elemento']
+   #aggiunge alla lista
+   if elemento:
+      lista_spesa.append(elemento)
+return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
