161e6b28448e8ca605fdd6efade14e606d317bdc
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-11-27 09:46:19 +0000

    punto 1 fase 3

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #inizializza l'app Flask
 app = Flask(__name__)
-
+lista_spesa= []
 #rotta principale
 @app.route('/')
 def home():
- return "Per ora funziona tutto"
+ return render_template('index.html')
 
 #avvio dell'app Flask
 if __name__ == '__main__':
