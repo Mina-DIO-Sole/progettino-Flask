501a981cd115c1fdbcc8a3502646d0f9e954f7ba
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-12-02 11:55:26 +0000

    fatto punto 2

--- app.py
+++ app.py
@@ -11,7 +11,18 @@ def aggiungi():
    #aggiunge alla lista
    if elemento:
       lista_spesa.append(elemento)
-return redirect(url_for('home'))
+   return redirect(url_for('home'))
+@app.route('/')
+def home():
+ return render_template('index.html')
+
+
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+   if 0 <= indice < len(lista_spesa):
+      lista_spesa.pop(indice)
+      return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
