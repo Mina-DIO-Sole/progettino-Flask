85a7235c20d6f9ee70e050e2e5f0ff0846a30d37
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-12-09 11:46:10 +0000

    punto 3

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 #inizializza l'app Flask
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+   db.create_all()
+
+
 lista_spesa= []
 #rotta principale
 @app.route('/aggiungi', methods=['POST'])
@@ -14,7 +22,7 @@ def aggiungi():
    return redirect(url_for('home'))
 @app.route('/')
 def home():
- return render_template('index.html')
+ return render_template('index.html', lista=lista_spesa)
 
 
 
@@ -23,6 +31,13 @@ def rimuovi(indice):
    if 0 <= indice < len(lista_spesa):
       lista_spesa.pop(indice)
       return redirect(url_for('home'))
+
+
+@app.route('/svuota', methods=['POST'])
+def svuota():
+    global lista_spesa  
+    lista_spesa.clear()  
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
