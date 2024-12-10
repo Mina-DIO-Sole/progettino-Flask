b1a13fccc6825420011fe54c900725a3583bf7b3
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-12-10 17:01:25 +0000

    tutto apposto e finito

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from models import db, ListaSpesa
+
 #inizializza l'app Flask
 app = Flask(__name__)
 
+#configura db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db.init_app(app)
+
+#crea il db nel caso non esistesse anora
 with app.app_context():
    db.create_all()
 
@@ -19,6 +25,8 @@ lista_spesa= []
 def home():
  return render_template('index.html', lista=lista_spesa)
 
+ 
+
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
    elemento = request.form['elemento']
@@ -30,8 +38,6 @@ def aggiungi():
 
 
 
-
-
 @app.route('/rimuovi/<int:indice>', methods=['POST'])
 def rimuovi(indice):
    if 0 <= indice < len(lista_spesa):
@@ -39,11 +45,6 @@ def rimuovi(indice):
       db.session.delete(elemento) 
       db.session.commit() 
       return redirect(url_for('home'))
-
-
-
-
-
 
 
 @app.route('/svuota', methods=['POST'])
--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy() #inizializziamo il con sqlalchemy
 
 
 
 
-class ListaSpesa(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class ListaSpesa(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
     elemento = db.Column(db.String(100), nullable=False)
